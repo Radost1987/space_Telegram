6fea89bbf6f9f815f6a8b0307617cf784059dc2a
Yuliya <yuliya_larionova@inbox.ru>
2021-09-05 10:31:33 +0300

    Images not be load

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -4,13 +4,6 @@ import requests
 from pathlib import Path
 
 
-def download_images(url, filename):
-    response = requests.get(url)
-    response.raise_for_status()
-    with open(os.path.join('SpaceX images', filename), 'wb') as file:
-        image = file.write(response.content)
-    return image
-
 
 def fetch_spacex_last_launch():
     url = 'https://api.spacexdata.com/v4/launches/latest'
